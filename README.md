903859bbd32a593de81502ce30279259b5797dda
Charles Moulliard <cmoulliard@apache.org>
2016-06-30 13:28:50 +0200

    Add jolokia fraction. Change packaging to jar

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+
+
+curl -X GET http://localhost:8080/jmx
+curl -d "{\"type\":\"read\",\"mbean\":\"java.lang:type=Memory\",\"attribute\":\"HeapMemoryUsage\",\"path\":\"used\"}" http://localhost:8080/jmx/ && echo ""
--- src/main/java/org/demo/MainApp.java
+++ src/main/java/org/demo/MainApp.java
@@ -3,6 +3,8 @@ package org.demo;
 import org.demo.route.RestService;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.camel.core.CamelCoreFraction;
+import org.wildfly.swarm.jolokia.JolokiaFraction;
+import org.wildfly.swarm.management.ManagementFraction;
 
 public class MainApp {
 
@@ -10,8 +12,29 @@ public class MainApp {
 		Swarm swarm = new Swarm();
 
 		// Add fractions
+
+		// Camel
 		swarm.fraction(new CamelCoreFraction()
 		        .addRouteBuilder(new RestService()));
+
+		// Jolokia - JMX HTTP Bridge
+		swarm.fraction(new JolokiaFraction("/jmx"));
+
+		/*
+		// Mngt Console
+		swarm.fraction( new ManagementFraction()
+				.httpInterfaceManagementInterface((iface) -> {
+					iface.allowedOrigin("http://localhost:8080");
+					iface.securityRealm("ManagementRealm");
+				})
+				.securityRealm("ManagementRealm", (realm) -> {
+					realm.inMemoryAuthentication( (authn)->{
+						authn.add( "bob", "tacos!", true );
+					});
+					realm.inMemoryAuthorization( (authz)->{
+						authz.add( "bob", "admin" );
+					});
+				}));*/
 
 		swarm.start();
 		swarm.deploy();
