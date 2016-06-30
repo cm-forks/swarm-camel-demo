3297c35f417851ca026a4faecb378dbbff0a9dd1
Charles Moulliard <cmoulliard@apache.org>
2016-06-30 17:52:15 +0200

    Clean up code

--- src/main/java/org/demo/MainApp.java
+++ src/main/java/org/demo/MainApp.java
@@ -21,30 +21,6 @@ public class MainApp {
 		// Jolokia Fraction - JMX HTTP Bridge
 		swarm.fraction(new JolokiaFraction("/jmx"));
 
-		/*
-		// Mngt Console
-		swarm.fraction( new ManagementFraction()
-				.httpInterfaceManagementInterface((iface) -> {
-					iface.allowedOrigin("http://localhost:8080");
-					iface.securityRealm("ManagementRealm");
-				})
-				.securityRealm("ManagementRealm", (realm) -> {
-					realm.inMemoryAuthentication( (authn)->{
-						authn.add( "bob", "tacos!", true );
-					});
-					realm.inMemoryAuthorization( (authz)->{
-						authz.add( "bob", "admin" );
-					});
-				}));*/
-
-/*		// Hawtio Web Console
-        WARArchive hawtWar = ShrinkWrap.create(WARArchive.class);
-		hawtWar.addAsWebResource(new URL("http://oss.sonatype.org/content/repositories/public/io/hawt/hawtio-default/1.4.65/hawtio-default-1.4.65.war"),"/");
-
-		WARArchive hawtWar = ShrinkWrap.createFromZipFile(WARArchive.class,new File("/Users/chmoulli/Temp/test-swarm/demo/src/hawtio-default-1.4.65.war"));
-		hawtWar.setContextRoot("/hawtio");
-*/
-
 		swarm.start();
 		// swarm.deploy(hawtWar);
 		swarm.deploy();
